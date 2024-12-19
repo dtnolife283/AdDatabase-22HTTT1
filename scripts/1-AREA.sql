6076949ec2fecdd35d7f2f3d83c2730315784d79
tiam4tt <thanhthienxgmh@gmail.com>
2024-12-19 23:30:55 +0700

    update 7.2

--- scripts/1-AREA.sql
+++ scripts/1-AREA.sql
@@ -1,5 +1,5 @@
-INSERT INTO AREA (ID_Area, AreaName) VALUES ('1', 'Anthonyhaven');
-INSERT INTO AREA (ID_Area, AreaName) VALUES ('2', 'West Vanessa');
-INSERT INTO AREA (ID_Area, AreaName) VALUES ('3', 'East Stephanieport');
-INSERT INTO AREA (ID_Area, AreaName) VALUES ('4', 'Hardyhaven');
-INSERT INTO AREA (ID_Area, AreaName) VALUES ('5', 'South Anaborough');
+INSERT INTO AREA (ID_Area, AreaName) VALUES ('1', 'Marcburgh');
+INSERT INTO AREA (ID_Area, AreaName) VALUES ('2', 'Dixonborough');
+INSERT INTO AREA (ID_Area, AreaName) VALUES ('3', 'Rodriguezmouth');
+INSERT INTO AREA (ID_Area, AreaName) VALUES ('4', 'West Briana');
+INSERT INTO AREA (ID_Area, AreaName) VALUES ('5', 'East Christine');
--- scripts/17-MEMBERSHIP_LEVEL.sql
+++ scripts/17-MEMBERSHIP_LEVEL.sql
@@ -1,3 +1,3 @@
-INSERT INTO MEM_LEVEL (ID_Level, LevelName, DiscountPercentage) VALUES (1, 'Bronze', 14);
-INSERT INTO MEM_LEVEL (ID_Level, LevelName, DiscountPercentage) VALUES (2, 'Silver', 11);
-INSERT INTO MEM_LEVEL (ID_Level, LevelName, DiscountPercentage) VALUES (3, 'Gold', 19);
+INSERT INTO MEM_LEVEL (ID_Level, LevelName, DiscountPercentages) VALUES (1, 'Bronze', 13);
+INSERT INTO MEM_LEVEL (ID_Level, LevelName, DiscountPercentages) VALUES (2, 'Silver', 9);
+INSERT INTO MEM_LEVEL (ID_Level, LevelName, DiscountPercentages) VALUES (3, 'Gold', 5);
