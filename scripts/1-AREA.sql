c5202ff4820dad2cd5b5302d31f9140679484f80
tiam4tt <thanhthienxgmh@gmail.com>
2024-12-19 23:24:11 +0700

    update 5

--- scripts/1-AREA.sql
+++ scripts/1-AREA.sql
@@ -1,5 +1,5 @@
-INSERT INTO AREA (ID_Area, AreaName) VALUES ('1', 'Romanland');
-INSERT INTO AREA (ID_Area, AreaName) VALUES ('2', 'Ashleyshire');
-INSERT INTO AREA (ID_Area, AreaName) VALUES ('3', 'Amandafurt');
-INSERT INTO AREA (ID_Area, AreaName) VALUES ('4', 'Lake Kathyfurt');
-INSERT INTO AREA (ID_Area, AreaName) VALUES ('5', 'Brentfurt');
+INSERT INTO AREA (ID_Area, AreaName) VALUES ('1', 'Fullerberg');
+INSERT INTO AREA (ID_Area, AreaName) VALUES ('2', 'West Heather');
+INSERT INTO AREA (ID_Area, AreaName) VALUES ('3', 'Port Christianshire');
+INSERT INTO AREA (ID_Area, AreaName) VALUES ('4', 'Shelbyport');
+INSERT INTO AREA (ID_Area, AreaName) VALUES ('5', 'New Daniel');
--- scripts/17-MEMBERSHIP_LEVEL.sql
+++ scripts/17-MEMBERSHIP_LEVEL.sql
@@ -1,3 +1,3 @@
-INSERT INTO MEMBERSHIP_LEVEL (ID_MemLevel, MemLevelName, Discount) VALUES (1, 'Bronze', 16);
-INSERT INTO MEMBERSHIP_LEVEL (ID_MemLevel, MemLevelName, Discount) VALUES (2, 'Silver', 11);
-INSERT INTO MEMBERSHIP_LEVEL (ID_MemLevel, MemLevelName, Discount) VALUES (3, 'Gold', 6);
+INSERT INTO MEMBERSHIP_LEVEL (ID_MemLevel, MemLevelName, Discount) VALUES (1, 'Bronze', 6);
+INSERT INTO MEMBERSHIP_LEVEL (ID_MemLevel, MemLevelName, Discount) VALUES (2, 'Silver', 17);
+INSERT INTO MEMBERSHIP_LEVEL (ID_MemLevel, MemLevelName, Discount) VALUES (3, 'Gold', 19);
