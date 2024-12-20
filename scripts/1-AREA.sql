da4f7045c36a42afc85afad4a9054d51c3168b05
tiam4tt <thanhthienxgmh@gmail.com>
2024-12-20 09:11:08 +0700

    update 14

--- scripts/1-AREA.sql
+++ scripts/1-AREA.sql
@@ -1,5 +1,5 @@
-INSERT INTO AREA (ID_Area, AreaName) VALUES (1, 'Edwardsburgh');
-INSERT INTO AREA (ID_Area, AreaName) VALUES (2, 'South Christopherhaven');
-INSERT INTO AREA (ID_Area, AreaName) VALUES (3, 'Hartbury');
-INSERT INTO AREA (ID_Area, AreaName) VALUES (4, 'Micheleburgh');
-INSERT INTO AREA (ID_Area, AreaName) VALUES (5, 'Christophertown');
+INSERT INTO AREA (ID_Area, AreaName) VALUES (1, 'East Gabriel');
+INSERT INTO AREA (ID_Area, AreaName) VALUES (2, 'West Shawntown');
+INSERT INTO AREA (ID_Area, AreaName) VALUES (3, 'Collierberg');
+INSERT INTO AREA (ID_Area, AreaName) VALUES (4, 'Hudsonland');
+INSERT INTO AREA (ID_Area, AreaName) VALUES (5, 'West Kathleenbury');
--- scripts/2-DEPARTMENT.sql
+++ scripts/2-DEPARTMENT.sql
@@ -1,7 +1,7 @@
-INSERT INTO DEPARTMENT (ID_Department, DepartmentName, Salary) VALUES (1, 'Management', 8000000);
-INSERT INTO DEPARTMENT (ID_Department, DepartmentName, Salary) VALUES (2, 'Kitchen', 14000000);
+INSERT INTO DEPARTMENT (ID_Department, DepartmentName, Salary) VALUES (1, 'Management', 11000000);
+INSERT INTO DEPARTMENT (ID_Department, DepartmentName, Salary) VALUES (2, 'Kitchen', 7500000);
 INSERT INTO DEPARTMENT (ID_Department, DepartmentName, Salary) VALUES (3, 'Cleaning', 17500000);
-INSERT INTO DEPARTMENT (ID_Department, DepartmentName, Salary) VALUES (4, 'Security', 9500000);
-INSERT INTO DEPARTMENT (ID_Department, DepartmentName, Salary) VALUES (5, 'Reception', 8000000);
-INSERT INTO DEPARTMENT (ID_Department, DepartmentName, Salary) VALUES (6, 'Accounting', 18000000);
-INSERT INTO DEPARTMENT (ID_Department, DepartmentName, Salary) VALUES (7, 'Service', 8000000);
+INSERT INTO DEPARTMENT (ID_Department, DepartmentName, Salary) VALUES (4, 'Security', 16000000);
+INSERT INTO DEPARTMENT (ID_Department, DepartmentName, Salary) VALUES (5, 'Reception', 7500000);
+INSERT INTO DEPARTMENT (ID_Department, DepartmentName, Salary) VALUES (6, 'Accounting', 15500000);
+INSERT INTO DEPARTMENT (ID_Department, DepartmentName, Salary) VALUES (7, 'Service', 19000000);
