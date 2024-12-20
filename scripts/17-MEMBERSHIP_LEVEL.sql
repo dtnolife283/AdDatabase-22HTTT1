5818e64fa40c7f3162956fc836b9e478c6bfa7db
dtnolife283 <tdtuan22@clc.fitus.edu.vn>
2024-12-20 21:25:55 +0700

    full

--- scripts/17-MEMBERSHIP_LEVEL.sql
+++ scripts/17-MEMBERSHIP_LEVEL.sql
@@ -1,3 +1,3 @@
-INSERT INTO MEM_LEVEL (ID_Level, LevelName, DiscountPercentages) VALUES (1, 'Bronze', 13);
-INSERT INTO MEM_LEVEL (ID_Level, LevelName, DiscountPercentages) VALUES (2, 'Silver', 11);
-INSERT INTO MEM_LEVEL (ID_Level, LevelName, DiscountPercentages) VALUES (3, 'Gold', 15);
+INSERT INTO MEM_LEVEL (ID_Level, LevelName, DiscountPercentages) VALUES (1, 'Bronze', 15);
+INSERT INTO MEM_LEVEL (ID_Level, LevelName, DiscountPercentages) VALUES (2, 'Silver', 7);
+INSERT INTO MEM_LEVEL (ID_Level, LevelName, DiscountPercentages) VALUES (3, 'Gold', 5);
