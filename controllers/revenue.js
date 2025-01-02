d6a2eb9b23cf3a071daf877a017d3a863eae586e
IaTOgN <taingotan0811@gmail.com>
2025-01-02 22:39:06 +0700

    revenue final

--- controllers/revenue.js
+++ controllers/revenue.js
@@ -13,6 +13,7 @@ const revenueController = {
                 revenueBy,
                 searchName
             });
+           
         } catch (err) {
             console.error("Error fetching statistics data:", err);
             res.status(500).send("Internal Server Error");
@@ -27,49 +28,60 @@ const revenueController = {
                 return res.status(400).send("Start date, end date, and revenue type are required.");
             }
 
-            let revenueData;
+            let revenueData = [];
             let isBranchView = revenueBy === "branches";
             if (isBranchView) {
 
             // Query to get revenue for each dish by branch within the date range
-                revenueData = await db("ORDER")
-                .join("ORDER_FOOD", "ORDER.ID_Order", "ORDER_FOOD.ID_Order")
-                .join("BRANCH_FOOD", "ORDER_FOOD.ID_BranchFood", "BRANCH_FOOD.ID_BranchFood")
-                .join("FOOD_ITEM", "BRANCH_FOOD.ID_Food", "FOOD_ITEM.ID_Food")
+            revenueData = await db("ORDER as O")
+                .join("BRANCH as B", "O.ID_Branch", "B.ID_Branch")
+                .join("BRANCH_FOOD as BF", "BF.ID_Branch", "O.ID_Branch")
+                .join("FOOD_ITEM as FI", "BF.ID_Food", "FI.ID_Food")
+                .join("ORDER_FOOD as ORF", function () {
+                    this.on("ORF.ID_Order", "O.ID_Order")
+                        .andOn("ORF.ID_BranchFood", "BF.ID_BranchFood");
+                })
+                .where("B.BranchName", searchName)
+                .whereBetween("O.OrderDate", [startDate, endDate])
                 .select(
-                    "BRANCH_FOOD.ID_Branch",
-                    "BRANCH.BranchName",
-                    "BRANCH_FOOD.ID_Food",
-                    "FOOD_ITEM.FoodName",
-                    db.raw("SUM(ORDER.ActualPrice) as TotalRevenue")
+                    "B.ID_Branch",
+                    "B.BranchName",
+                    "FI.FoodName",
+                    db.raw("SUM(FI.Price * ORF.Quantity) as TotalRevenue")
                 )
-                .whereLike("BRANCH.BranchName", searchName)
-                .whereBetween("ORDER.OrderDate", [startDate, endDate])
-                .groupBy("BRANCH_FOOD.ID_Branch", "BRANCH_FOOD.ID_Food")
-                .orderBy("TotalRevenue", "desc");
+                .groupBy("B.ID_Branch", "B.BranchName", "FI.FoodName", "FI.ID_Food")
+                .orderBy("TotalRevenue", "asc");
             }
             else {
                 // Query to get revenue for each dish by area within the date range
-                revenueData = await db("ORDER")
-                .join("ORDER_FOOD", "ORDER.ID_Order", "ORDER_FOOD.ID_Order")
-                .join("BRANCH_FOOD", "ORDER_FOOD.ID_BranchFood", "BRANCH_FOOD.ID_BranchFood")
-                .join("BRANCH", "ORDER.ID_Branch", "BRANCH.ID_Branch")
-                .join("FOOD_ITEM", "BRANCH_FOOD.ID_Food", "FOOD_ITEM.ID_Food")
-                .join("AREA", "BRANCH.ID_Area", "AREA.ID_Area")
+                revenueData = await db("ORDER as O")
+                .join("BRANCH as B", "O.ID_Branch", "B.ID_Branch")
+                .join("BRANCH_FOOD as BF", "BF.ID_Branch", "O.ID_Branch")
+                .join("AREA as A", "A.ID_Area", "B.ID_Area")
+                .join("FOOD_ITEM as FI", "BF.ID_Food", "FI.ID_Food")
+                .join("ORDER_FOOD as ORF", function () {
+                    this.on("ORF.ID_Order", "O.ID_Order")
+                        .andOn("ORF.ID_BranchFood", "BF.ID_BranchFood");
+                })
+                .where("A.AreaName", searchName)
+                .whereBetween("O.OrderDate", [startDate, endDate])
                 .select(
-                    "BRANCH.ID_Area",
-                    "AREA.AreaName",
-                    "BRANCH.BranchName",
-                    "BRANCH_FOOD.ID_Food",
-                    "FOOD_ITEM.FoodName",
-                    db.raw("SUM(ORDER.ActualPrice) as TotalRevenue")
+                    "B.ID_Branch",
+                    "B.BranchName",
+                    "FI.FoodName",
+                    db.raw("SUM(FI.Price * ORF.Quantity) as TotalRevenue")
                 )
-                .whereLike("AREA.AreaName", searchName)
-                .whereBetween("ORDER.OrderDate", [startDate, endDate])
-                .groupBy("BRANCH.ID_Area", "BRANCH_FOOD.ID_Food")
-                .orderBy("TotalRevenue", "desc");
+                .groupBy("A.ID_Area", "B.ID_Branch", "B.BranchName", "FI.FoodName", "FI.ID_Food")
+                .orderBy("TotalRevenue", "asc");
             }
 
+            console.log(req.query);
+            revenueData = revenueData.map((row) => {
+                return {
+                    ...row,
+                    totalRevenue: new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(row.TotalRevenue)
+                };
+            });
             // Render the results to the page
             res.render("revenue", {
                 customCSS: ['online_user_home.css'],
